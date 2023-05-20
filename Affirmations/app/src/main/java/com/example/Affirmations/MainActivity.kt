f1d5a78de5574af66c94faa2a08c6719eb8d6ab2
Nguyễn Mạnh Cường <132721760+nguyenmanhcuong2810@users.noreply.github.com>
2023-05-20 12:49:21 +0700

    affirmation

--- Affirmations/app/src/main/java/com/example/Affirmations/MainActivity.kt
+++ Affirmations/app/src/main/java/com/example/Affirmations/MainActivity.kt
@@ -19,6 +19,6 @@ class MainActivity : AppCompatActivity() {
         recyclerView.adapter = ItemAdapter(this, myDataset)
 
         recyclerView.setHasFixedSize(true)
-        val temp = 0
+
     }
 }
