40bee969d58881d99e43e64750d53b792bd60b9d
Nguyễn Mạnh Cường <132721760+nguyenmanhcuong2810@users.noreply.github.com>
2023-05-20 12:39:31 +0700

    Affirmations

---
+++ Affirmations/app/src/main/java/com/example/Affirmations/data/Datasource.kt
@@ -0,0 +1,21 @@
+package com.example.Affirmations.data
+import com.example.Affirmations.model.Affirmation
+import com.example.affirmations.R
+
+class Datasource {
+
+    fun loadAffirmations(): List<Affirmation> {
+        return listOf<Affirmation>(
+            Affirmation(R.string.affirmation1),
+            Affirmation(R.string.affirmation2),
+            Affirmation(R.string.affirmation3),
+            Affirmation(R.string.affirmation4),
+            Affirmation(R.string.affirmation5),
+            Affirmation(R.string.affirmation6),
+            Affirmation(R.string.affirmation7),
+            Affirmation(R.string.affirmation8),
+            Affirmation(R.string.affirmation9),
+            Affirmation(R.string.affirmation10)
+        )
+    }
+}
